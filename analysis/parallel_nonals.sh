53d72668991e717bd89ea9fee5b7b95cb2e87641
Divya Ramamoorthy <25695820+divyaramamoorthy@users.noreply.github.com>
2022-02-13 17:36:10 -0500

    slurm nonals domain

---
+++ analysis/parallel_nonals.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# proj_list=('adni' 'ppmi')
+# seed_list=(0 1 2 3 4)
+kernel_list=('rbf')
+proj_list=('adni')
+seed_list=(0)
+
+for kernel in "${kernels_list[@]}" ; do
+	for seed in "${seed_list[@]}" ; do
+		for proj in "${proj_list[@]}" ; do
+	    			sbatch run_nonals.sbatch -p $proj -s $seed -k $kernel
+		done
+	done
+done
