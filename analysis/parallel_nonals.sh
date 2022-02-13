a0b55739ce0c25a8c91f0592e48eccadc60aeeee
Divya Ramamoorthy <25695820+divyaramamoorthy@users.noreply.github.com>
2022-02-13 17:43:12 -0500

    full param set

--- analysis/parallel_nonals.sh
+++ analysis/parallel_nonals.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# proj_list=('adni' 'ppmi')
-# seed_list=(0 1 2 3 4)
+proj_list=('ppmi' 'adni')
+seed_list=(0 1 2 3 4)
 kernels_list=('rbf')
-proj_list=('adni')
-seed_list=(0)
+#proj_list=('adni')
+#seed_list=(0)
 
 for kernel in "${kernels_list[@]}" ; do
 	for seed in "${seed_list[@]}" ; do
