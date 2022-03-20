c10afd6626729c77b702373d295ecdef429d1c25
Divya Ramamoorthy <25695820+divyaramamoorthy@users.noreply.github.com>
2022-03-19 23:53:50 -0500

    run full nathist mogp

---
+++ analysis/parallel_nathist_full.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+exp='full'
+proj='nathist'
+seed_list=(0 1 2 3 4)
+kernel='rbf'
+run_by_seed=true
+
+# Don't need below; using to fit pre-existing sbatch
+task=1.0
+alphasc=1.0
+minnum='min3'
+
+for seed in "${seed_list[@]}" ; do
+    sbatch run_predict_alphavar.sbatch -a $exp -b $proj -c $kernel -d $run_by_seed -e $seed -f $task -g $alphasc -h $minnum
+done
