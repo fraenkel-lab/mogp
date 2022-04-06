6a9057b3677e0a3eabd0cd1d512601207d9879e2
Divya Ramamoorthy <25695820+divyaramamoorthy@users.noreply.github.com>
2022-04-05 20:49:51 -0400

    nathist linear kernel

--- analysis/parallel_nathist_full.sh
+++ analysis/parallel_nathist_full.sh
@@ -3,14 +3,16 @@
 exp='full'
 proj='nathist'
 seed_list=(0 1 2 3 4)
-kernel='rbf'
+#kernel='rbf'
+kernel='linear'
 run_by_seed=true
 
 # Don't need below; using to fit pre-existing sbatch
 task=1.0
 alphasc=1.0
 minnum='min3'
+expname='nathistfull'
 
 for seed in "${seed_list[@]}" ; do
-    sbatch run_predict_alphavar.sbatch -a $exp -b $proj -c $kernel -d $run_by_seed -e $seed -f $task -g $alphasc -h $minnum
+    sbatch run_predict_alphavar.sbatch -a $exp -b $proj -c $kernel -d $run_by_seed -e $seed -f $task -g $alphasc -h $minnum -i $expname
 done
