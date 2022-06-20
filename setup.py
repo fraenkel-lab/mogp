87e37590d443d28c893370ce875d0e2cdd36ca91
Divya Ramamoorthy <25695820+divyaramamoorthy@users.noreply.github.com>
2022-06-20 00:04:03 -0400

    fix dependency issue by narrowly specifying Python version (3.7.3)

--- setup.py
+++ setup.py
@@ -2,20 +2,20 @@ from setuptools import setup
 
 setup(
     name='mogp',
-    version='0.1.1',
+    version='0.1.2',
     description='Mixture of Gaussian Processes Model for Sparse Longitudinal Data',
     url='https://github.com/fraenkel-lab/mogp',
-    python_requires='>=3.6',
+    python_requires='3.7.3',
     packages=['mogp'],
     # package_data={'mogp': ['data/reference_model.pkl']},
 
-    install_requires=[
-        'GPy>=1.9.8,<1.10',
-        'scipy>=1.3.0',
-        'numpy>=1.16.4,<1.20',
-        'scikit-learn==0.21.1',
-        'sklearn>=0.0',
-        'matplotlib>=3.1.1'],
+#    install_requires=[
+#        'GPy>=1.9.8,<1.10',
+#        'scipy>=1.3.0',
+#        'numpy>=1.16.4,<1.20',
+#        'scikit-learn==0.21.1',
+#        'sklearn>=0.0',
+#        'matplotlib>=3.1.1'],
 
     author='Divya Ramamoorthy',
     author_email='divyar@mit.edu',
